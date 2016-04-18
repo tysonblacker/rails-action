db72b3892f3337c66ec2f996a9f8d37507e757d1
Tyson Blacker <tyson.blacker@gmail.com>
2016-04-18 08:21:20 +0000

    Added admin flag to User Model and, seeded the first admin user

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,8 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
+  belongs_to :author, class_name: "User"
+  
+  
   validates :name, presence: true
   validates :description, presence: true
   validates :description, presence: true, length: {minimum: 10}
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,8 +29,8 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   
   config.include Warden::Test::Helpers, type: :feature
+  config.include Warden::Test::Helpers, type: :controller
   config.after(type: :feature) { Warden.test_reset! }
-  
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
